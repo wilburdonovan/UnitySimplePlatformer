820dc75cbf9a5c775025e1447a8b7698f9ae5136
Wilbur Donovan <wilbur.donovan91@gmail.com>
2019-05-13 21:16:15 +1000

    Basic functionality/ first commit

---
+++ Assets/BayatGames/Free Platform Game Assets/Character Animation/Idle/Move2D.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool facingRight = true;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        moveInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        } else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+	}
+
+    void Flip ()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ Assets/scripts/CaracterMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaracterMove : MonoBehaviour {
+
+    public float speed;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        moveInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveInput * speed , rb.velocity.y);
+	}
+}
